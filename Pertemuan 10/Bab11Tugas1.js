be54983000514014708e07e45ba697eded410d14
Hafidh Dzaki <24hafidhdzaki@gmail.com>
2026-06-13 11:16:23 +0700

    Upload Tugas Pertemuan 10

---
+++ Pertemuan 10/Bab11Tugas1.js
@@ -0,0 +1,8 @@
+let nilai1 = Number(prompt("Masukkan bilangan pertama"));
+let nilai2 = Number(prompt("Masukkan bilangan kedua"));
+
+if (nilai1 > nilai2){
+    document.write("BILANGAN KE-1 LEBIH BESAR DARI BILANGAN KE-2");
+} else {
+    document.write("BILANGAN KE-2 LEBIH BESAR DARI BILANGAN KE-1");
+}
---
+++ Pertemuan 10/script.js
@@ -0,0 +1,1 @@
+alert("Ini adalah kode eksternal javascript");
